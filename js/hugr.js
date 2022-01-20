3b2fec3d9df6196028e2ef49180f441499bedaf7
adelya-shakirvoa <96436304+adelya-shakirvoa@users.noreply.github.com>
2022-01-20 21:05:01 +0500

    Add files via upload

--- js/hugr.js
+++ js/hugr.js
@@ -41,3 +41,46 @@ $("a.scroll-to").on("click", function(e) {
 $("nav a.scroll-to").on("click", function(e) {
     toggleNav();
 });
+
+$(document).ready(function() {
+    // Cookie
+    let cookie = false;
+    let cookieContent = $('.cookie-disclaimer');
+
+    checkCookie();
+
+    if (cookie === true) {
+        cookieContent.hide();
+    }
+
+    function setCookie(cname, cvalue, exdays) {
+        let d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toGMTString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
+
+    function getCookie(cname) {
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i].trim();
+            if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+        }
+        return "";
+    }
+
+    function checkCookie() {
+        let check = getCookie("acookie");
+        if (check !== "") {
+            return cookie = true;
+        } else {
+            return cookie = false; //setCookie("acookie", "accepted", 365);
+        }
+    }
+
+    $('.accept-cookie').click(function () {
+        setCookie("acookie", "accepted", 365);
+        cookieContent.hide(500);
+    });
+});
